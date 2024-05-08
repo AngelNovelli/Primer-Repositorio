3ca628771ddf4c65ff328a1a98a6e54c2f3ff56c
AngelNovelli <angeljesusnovelli@gmail.com>
2024-05-08 19:36:13 -0300

    Mi segundo commit.

---
+++ holaMundo.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var rls = require("readline-sync");
+var numero1 = rls.questionInt("Ingrese un numero: ");
+var numero2 = rls.questionInt("Ingrese un numero: ");
+var resultado = 0;
+resultado = numero1 + numero2;
+console.log("Hola mundo" + resultado);
--- holaMundo.ts
+++ holaMundo.ts
@@ -1,1 +1,11 @@
-console.log ("Hola mundo");
+import * as rls from 'readline-sync';
+
+let numero1 : number = rls.questionInt ("Ingrese un numero: ");
+let numero2 : number = rls.questionInt ("Ingrese un numero: ");
+let resultado : number = 0;
+
+resultado = numero1 + numero2
+
+console.log ("Hola mundo" + resultado);
+
+
